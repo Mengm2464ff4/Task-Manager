9f01c129f21aecb81915e48a94b24c5da5cb64d2
Mengm2464ff4 <SongMeng66667777@gmail.com>
2026-04-28 15:43:14 +0700

    Fix Layout import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import Home from './pages/Home';
-import Navbar from './components/layout/Navbar';
+import Navbar from './components/layout/Navbar.jsx';
 import { TaskProvider } from './context/TaskContext';
 import { useDarkMode } from './hooks/useDarkMode';
 import { useMouseEffect } from './hooks/useMouseEffect';
